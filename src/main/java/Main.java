78608410dee31beaa2c3916d9306dbec08119635
nehoraii <nehoraii555@gmail.com>
2023-01-25 22:03:43 +0200

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+import entities.*;
+import jakarta.persistence.*;
+
+//import jakarta.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hdjfj");
+        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager=entityManagerFactory.createEntityManager();
+        EntityTransaction transaction=entityManager.getTransaction();
+        try {
+            transaction.begin();
+            MyTableNameEntity dataEntity=new MyTableNameEntity();
+            dataEntity.setFirstName("add of jpa");
+            entityManager.persist(dataEntity);
+            transaction.commit();
+        }
+        finally {
+            if (transaction.isActive()){
+                transaction.rollback();
+            }
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
 }
